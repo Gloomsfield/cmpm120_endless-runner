f9f8ec714d96356e0a17b7f0e8f805abf1a3457e
Gloomsfield <amory@acosta.boo>
2026-02-14 15:28:25 -0800

    fix formatting

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -5,16 +5,14 @@ class Player extends Object3D {
 			config
 		);
 
-		this.head = this.add_child_deferred(
-			{
-				object_class: PlayerHead,
-				config: {
-					local_position: new Phaser.Math.Vector3(0.0, 0.0, 0.0),
-					local_rotation: new Phaser.Math.Quaternion().identity(),
-					radius: 0.25,
-				}
-			},
-		);
+		this.head = this.add_child_deferred({
+			object_class: PlayerHead,
+			config: {
+				local_position: new Phaser.Math.Vector3(0.0, 1.5, 0.0),
+				local_rotation: new Phaser.Math.Quaternion().identity(),
+				radius: 0.25,
+			}
+		});
 	}
 }
 
