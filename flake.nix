ced51d3075d986ac8628e46067ac46bbef22386a
Gloomsfield <amory@acosta.boo>
2026-02-18 17:22:40 -0800

    add flake.nix because git is complaining

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+	description = "cmpm120_endless-runner development environment";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs }:
+		let
+			system = "aarch64-linux";
+			pkgs = import nixpkgs { inherit system; };
+		in {
+			devShells.aarch64-linux.default = pkgs.mkShell {
+				name = "cmpm120_endless-runner";
+
+				buildInputs = with pkgs; [
+					live-server
+					typescript-language-server
+				];
+
+				shellHook = ''
+					alias gitclockout='git commit --allow-empty -m "clocking out"'
+				'';
+			};
+		};
+}
